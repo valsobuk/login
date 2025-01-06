2ffc4cde6574a698e1cadca3945fca8db4a5ff39
valsobuk <orendacjakubnoel@gmail.com>
2025-01-06 17:51:00 +0100

    edited

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,60 +1,73 @@
 import React, { useEffect, useState } from "react";
-import Image from "../assets/image.png";
-import Logo from "../assets/logo.png";
+import Image from "../assets/image2.jpg";
+import Logo from "../assets/logo3.png";
 import GoogleSvg from "../assets/icons8-google.svg";
 import { FaEye } from "react-icons/fa6";
 import { FaEyeSlash } from "react-icons/fa6";
 
-
-
 const Login = () => {
-  const [ showPassword, setShowPassword ] = useState(false);
-
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="login-main">
-      <div className="login-left">
-        <img src={Image} alt="" />
-      </div>
+      <div
+        className="login-left"
+        style={{
+          width: "30%",
+          height: "100vh",
+          backgroundImage: `url(${Image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
       <div className="login-right">
         <div className="login-right-container">
           <div className="login-logo">
             <img src={Logo} alt="" />
           </div>
           <div className="login-center">
-            <h2>Welcome back!</h2>
-            <p>Please enter your details</p>
+            <h2>Vitajte naspäť!</h2>
+            <p>prosím vyplňte svoje údaje</p>
             <form>
               <input type="email" placeholder="Email" />
               <div className="pass-input-div">
-                <input type={showPassword ? "text" : "password"} placeholder="Password" />
-                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
-                
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                />
+                {showPassword ? (
+                  <FaEyeSlash
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  />
+                ) : (
+                  <FaEye
+                    onClick={() => {
+                      setShowPassword(!showPassword);
+                    }}
+                  />
+                )}
               </div>
 
               <div className="login-center-options">
                 <div className="remember-div">
                   <input type="checkbox" id="remember-checkbox" />
-                  <label htmlFor="remember-checkbox">
-                    Remember for 30 days
-                  </label>
+                  <label htmlFor="remember-checkbox">Zapamätať na 30 dní</label>
                 </div>
                 <a href="#" className="forgot-pass-link">
-                  Forgot password?
+                  Zabudli ste svoje heslo?
                 </a>
               </div>
               <div className="login-center-buttons">
-                <button type="button">Log In</button>
-                <button type="button">
-                  <img src={GoogleSvg} alt="" />
-                  Log In with Google
-                </button>
+                <button type="button">Prihlasiť sa</button>
               </div>
             </form>
           </div>
 
           <p className="login-bottom-p">
-            Don't have an account? <a href="#">Sign Up</a>
+            Nemáte ešte účet? <a href="#">Kontaktujte nás</a>
           </p>
         </div>
       </div>
